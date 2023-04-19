d4c3dabbcc38e32162447c6e14aa44031591296f
manalzareen <81857933+manalzareen@users.noreply.github.com>
2023-04-20 00:35:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+
+articles_data = pd.read_csv('articles.csv')
+all_articles = articles_data[['url' , 'title' , 'text' , 'lang' , 'total_events']]
+liked_articles = []
+not_liked_articles = []
+
+app = Flask(__name__)
+
+def assign_val():
+    m_data = {
+        "url": all_articles.iloc[0,0],
+        "title": all_articles.iloc[0,1],
+        "text": all_articles.iloc[0,2] or "N/A",
+        "lang": all_articles.iloc[0,3],
+        "total_events": all_articles.iloc[0,4]
+    }
+    return m_data
+
+# API to display first article
+@app.route("/get-article")
+def get_article():
+
+    return jsonify({
+        "data":all_articles[0]
+        "status":success
+    })
+
+
+# API to move the article into liked articles list
+@app.route("/liked-article" ,methods=["POST"])
+def liked_article():
+
+    return 
+    global all_articles
+    a=all_articles[0]
+    allarticles =all_articles[1:]
+    allarticles.append(m)
+    jsonify({
+        "status":"success"
+    })200
+
+
+
+# # API to move the article into not liked articles list
+@app.route("/unliked-article", methods=["POST"])
+def unliked_article():
+
+    return global all_articles
+    a=all_articles[0]
+    allarticles =all_articles[1:]
+    allarticles.append(m)
+    jsonify({
+        "status":"success"
+    })200
+
+# run the application
+if __name__ == "__main__":
+    app.run()
